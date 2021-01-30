8611f0335d677051116b22a14770ca01987dea11
thduc <thduc.17it3@sict.udn.vn>
2021-01-30 14:51:54 +0700

    fix follow

--- src/main/java/com/thduc/instafake/repository/FollowRepository.java
+++ src/main/java/com/thduc/instafake/repository/FollowRepository.java
@@ -19,7 +19,7 @@ public interface FollowRepository extends JpaRepository<Follows, Long> {
     boolean existsByFrom_IdAndTo_Id(long myId, long id);
 
     @Query(value = "SELECT user.id,user.avatar,user.username,user.fullname, CASE WHEN t.id then 1 ELSE 0 END as following from (SELECT * from follows WHERE follows.from_id = :myIdParam GROUP BY to_id) as t RIGHT JOIN user ON t.to_id = `user`.id " +
-            " WHERE user.id in ( SELECT to_id FROM follows WHERE follows.from_id = :idParam  GROUP BY to_id ) AND user.id != :myIdParam"
+            " WHERE user.id in ( SELECT to_id FROM follows WHERE follows.from_id = :idParam  GROUP BY to_id ) AND user.id != :idParam"
 //            + "LIMIT :offset , :limit"
             , nativeQuery = true)
     List<UserWithFollow> viewFollowingsUser(@Param("idParam") Long id, @Param("myIdParam") Long myId );
@@ -33,7 +33,7 @@ public interface FollowRepository extends JpaRepository<Follows, Long> {
     }
 
     @Query(value = "SELECT user.id,user.avatar,user.username,user.fullname, CASE WHEN t.id then 1 ELSE 0 END as following from (SELECT * from follows WHERE follows.from_id = :myIdParam GROUP BY to_id) as t RIGHT JOIN user ON t.to_id = `user`.id " +
-            " WHERE user.id in ( SELECT from_id FROM follows WHERE follows.to_id = :idParam  GROUP BY to_id ) AND user.id != :myIdParam"
+            " WHERE user.id in ( SELECT from_id FROM follows WHERE follows.to_id = :idParam  GROUP BY to_id ) AND user.id != :idParam"
 //            + "LIMIT :offset , :limit"
             , nativeQuery = true)
     List<UserWithFollow> viewFollowersUser(@Param("idParam") Long id, @Param("myIdParam") Long myId );
